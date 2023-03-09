fedd3474cbaf2ca2ab00da09fa0b56707d582cce
denys-revera <denys.revera@onseo.biz>
2023-03-09 22:48:04 +0200

    init

--- src/BotCommand/botOnSwitchState.js
+++ src/BotCommand/botOnSwitchState.js
@@ -1,5 +1,5 @@
 import { CHAT_ID } from '../../config.js';
 
 export default (bot, text) => {
-  bot.telegram.sendMessage(CHAT_ID, text).then(() => console.log(text));
+  bot.telegram.sendMessage(CHAT_ID, text);
 };
