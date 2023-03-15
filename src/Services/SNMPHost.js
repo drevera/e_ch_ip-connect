4819cedc35a0dfb2dae21887fa77062858ea8341
denys-revera <denys.revera@onseo.biz>
2023-03-15 17:27:55 +0200

    init

--- src/Services/SNMPHost.js
+++ src/Services/SNMPHost.js
@@ -22,22 +22,21 @@ export default (snmp, bot) => {
     const oid = `1.3.6.1.2.1.2.2.1.8.${oids[currentIp].oid}`;
     session.get([oid], (error, varbinds) => {
       if (error) {
-        console.log(item);
         if (item !== 0) {
           botOnSwitchState(bot, `${oid} ${oids[currentIp].location} connection error. Details: ${error}`);
-          item = 0;
+          item[currentIp] = 0;
         }
       } else {
         for (let i = 0; i < varbinds.length; i++) {
           if (varbinds[0]?.value === 2) {
             if (item !== 2) {
               botOnSwitchState(bot, `${oid} ${oids[currentIp].location} down`);
-              item = 2;
+              item[currentIp] = 2;
             }
           } else {
             if (item !== 1) {
               botOnSwitchState(bot, `${oid} ${oids[currentIp].location} up`);
-              item = 1;
+              item[currentIp] = 1;
             }
           }
         }
